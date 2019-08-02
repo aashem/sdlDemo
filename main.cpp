434421a499bed97dee8468d9f9ec6331355e883d
aashem <shemeikka.asla@gmail.com>
2019-08-02 16:15:13 +0300

    testing

--- main.cpp
+++ main.cpp
@@ -3,51 +3,54 @@
 
 using namespace std;
 
-int Init_SDL(){
-    if(SDL_Init(SDL_INIT_VIDEO) != 0){
-        cout << "Error initializing" << SDL_GetError() << endl;
-        return 0;
-    };
-    cout << "init completed" << endl;
 
-    SDL_Window *window = SDL_CreateWindow("testing", 100, 100, 640, 480, SDL_WINDOW_SHOWN );
-    if (window == nullptr){
-        cout << "error initializing window exiting..." << SDL_GetError() << endl;
-        SDL_Quit();
-        return 0;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == nullptr){
-        SDL_DestroyWindow(window);
-        cout << "error initializing renderer exiting..." << SDL_GetError() << endl;
-        SDL_Quit();
-        return 0;
-    }
-
-    string imagePath = "flower.bmp";
-    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
-    if(bmp == nullptr){
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        cout << "error initializing image exiting..." << SDL_GetError() << endl;
-        return 0;
-    }
-
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, bmp);
-    SDL_FreeSurface(bmp);
-
-    for(int i=0; i < 3;  ++i){
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(1000);
-    }
-}
 
 int main()
 {
-    Init_SDL();
+    int x = 300;
+    int y = 300;
+    SDL_Surface *screen, *temp, *sprite;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("game", 100, 100, 640, 480, 0);
+
+        if (window == nullptr){
+            cout << "window init failed";
+        }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (renderer == nullptr){
+            cout << "renderer init failed";
+       
+        }
+        SDL_bool done = SDL_FALSE;
+
+        while (!done){
+            SDL_Event event;
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+            SDL_RenderDrawLine(renderer, x, y, x+100, y+100);
+            SDL_RenderPresent(renderer);
+
+            ++x;
+            ++y;
+            while(SDL_PollEvent(&event)){
+                if(event.type == SDL_QUIT){
+                    done = SDL_TRUE;
+                }
+            }
+        }
+        if(renderer){
+            SDL_DestroyRenderer(renderer);
+        }
+        if(window){
+            SDL_DestroyWindow(window);
+        }
+ 
+
+  
+    SDL_Quit();
     return 0;
 }
